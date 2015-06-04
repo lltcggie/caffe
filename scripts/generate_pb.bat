8f86c28e7fe80944f021c629d7f11dd780da3fb2
lltcggie <lltcggie@gmail.com>
2015-06-05 02:29:52 +0900

    caffe.pbの生成スクリプトでファイルが未生成の場合ファイルが生成されないバグがあったのを修正

--- scripts/generate_pb.bat
+++ scripts/generate_pb.bat
@@ -2,42 +2,53 @@
 
 cd /d "%~dp0"
 
+set FILE1="../src/caffe/proto/caffe.proto"
+set FILE_TIMESTAMP="../msvc/libcaffe/caffe.proto.timestamp.txt"
+set TIMESTAMP_NEW=""
 set TIMESTAMP_OLD=""
 
-if exist "../msvc/libcaffe/caffe.pb.h.timestamp.txt" (
+for %%a in (%FILE1%) do set TIMESTAMP_NEW=%%~ta
+
+if exist "../msvc/libcaffe/caffe.proto.timestamp.txt" (
     goto timestamp_exist;
 ) else (
     goto break;
 )
 
 :timestamp_exist
-for /F "delims=# eol=#" %%A in (../msvc/libcaffe/caffe.pb.h.timestamp.txt) do (
+for /F "delims=# eol=#" %%A in (../msvc/libcaffe/caffe.proto.timestamp.txt) do (
     set TIMESTAMP_OLD=%%A
     goto break;
 )
 :break
 
-if exist "../src/caffe/proto/caffe.pb.h" (
+if exist "../src/caffe/proto/caffe.proto" (
     goto caffe_pb_h_exist;
 ) else (
-    goto caffe_pb_h_not_exist;
+    goto caffe_pb_h_generate;
 )
 
 :caffe_pb_h_exist
 
-set FILE1="../src/caffe/proto/caffe.pb.h"
-set FILE_TIMESTAMP="../msvc/libcaffe/caffe.pb.h.timestamp.txt"
-set TIMESTAMP_NEW=""
-
-for %%a in (%FILE1%) do set TIMESTAMP_NEW=%%~ta
 if "%TIMESTAMP_OLD%" NEQ "%TIMESTAMP_NEW%" (
-    echo caffe.pb.h is being generated
-    "../3rdparty/bin/protoc" -I="../src/caffe/proto" --cpp_out="../src/caffe/proto" "../src/caffe/proto/caffe.proto"
-    echo %TIMESTAMP_NEW%> %FILE_TIMESTAMP%
+    goto caffe_pb_h_generate;
 ) else (
-    echo caffe.pb.h remains the same as before
+    goto caffe_pb_h_no_generate;
 )
 
-:caffe_pb_h_not_exist
+:caffe_pb_h_generate
 
+echo caffe.proto is being generated
+"../3rdparty/bin/protoc" -I="../src/caffe/proto" --cpp_out="../src/caffe/proto" "../src/caffe/proto/caffe.proto"
+echo %TIMESTAMP_NEW%> %FILE_TIMESTAMP%
+
+goto caffe_pb_h_end;
+
+:caffe_pb_h_no_generate
+
+echo caffe.proto remains the same as before
+
+goto caffe_pb_h_end;
+
+:caffe_pb_h_end
 echo %~nx0 end
