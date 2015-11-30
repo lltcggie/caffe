4e5ee87d715a960b0c6122ec14ee0cafd0dbfcba
lltcggie <lltcggie@gmail.com>
2015-11-30 23:16:52 +0900

    Avoid the CMake bug

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -31,6 +31,23 @@ find_package(HDF5 COMPONENTS C HL static REQUIRED)
 include_directories(SYSTEM ${HDF5_INCLUDE_DIRS} ${HDF5_HL_INCLUDE_DIR})
 list(APPEND Caffe_LINKER_LIBS ${HDF5_C_STATIC_LIBRARY} ${HDF5_HL_STATIC_LIBRARY})
 
+# Avoid the CMake bug http://www.itk.org/Bug/print_bug_page.php?bug_id=1101
+get_property(_HDF5_C_INTERFACE_LINK_LIBRARIES
+             TARGET   ${HDF5_C_STATIC_LIBRARY}
+             PROPERTY INTERFACE_LINK_LIBRARIES)
+if("${_HDF5_C_INTERFACE_LINK_LIBRARIES}" MATCHES ".*zlib.*")
+  foreach(LIB ${_HDF5_C_INTERFACE_LINK_LIBRARIES})
+    find_package(${LIB} QUIET CONFIG)
+    if(NOT ${${LIB}_FOUND})
+      find_package(${LIB} QUIET)
+    endif()
+  endforeach()
+  find_package(zlib QUIET CONFIG)
+  if(NOT zlib_FOUND)
+    find_package(zlib QUIET)
+  endif()
+endif()
+
 # ---[ LMDB
 if(USE_LMDB)
   find_package(LMDB REQUIRED)
