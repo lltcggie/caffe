7e889daab7c1045dc6490c7c23c35a2d35b28d1f
lltcggie <lltcggie@gmail.com>
2016-10-14 17:44:34 +0900

    リンクエラーが発生するのを修正

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -27,9 +27,28 @@ list(APPEND Caffe_LINKER_LIBS ${GFLAGS_LIBRARIES})
 include(cmake/ProtoBuf.cmake)
 
 # ---[ HDF5
-find_package(HDF5 COMPONENTS HL REQUIRED)
+find_package(HDF5 COMPONENTS C HL static REQUIRED)
 include_directories(SYSTEM ${HDF5_INCLUDE_DIRS} ${HDF5_HL_INCLUDE_DIR})
-list(APPEND Caffe_LINKER_LIBS ${HDF5_LIBRARIES} ${HDF5_HL_LIBRARIES})
+list(APPEND Caffe_LINKER_LIBS ${HDF5_C_STATIC_LIBRARY} ${HDF5_HL_STATIC_LIBRARY})
+
+# Avoid the CMake bug http://www.itk.org/Bug/print_bug_page.php?bug_id=1101
+get_property(_HDF5_C_INTERFACE_LINK_LIBRARIES
+             TARGET   ${HDF5_C_STATIC_LIBRARY}
+             PROPERTY INTERFACE_LINK_LIBRARIES)
+if("${_HDF5_C_INTERFACE_LINK_LIBRARIES}" MATCHES ".*zlib.*")
+  foreach(LIB ${_HDF5_C_INTERFACE_LINK_LIBRARIES})
+    find_package(${LIB} QUIET CONFIG)
+    if(NOT ${${LIB}_FOUND})
+      find_package(${LIB} QUIET)
+    endif()
+  endforeach()
+  if(NOT ZLIB_FOUND)
+    find_package(zlib QUIET CONFIG)
+    if(NOT ZLIB_FOUND)
+      find_package(zlib QUIET)
+    endif()
+  endif()
+endif()
 
 # ---[ LMDB
 if(USE_LMDB)
